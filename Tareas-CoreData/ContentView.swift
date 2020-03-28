71b2089b5a861f7aa124e812cd631ec566284246
David Cantu <david.cantu.delgado@tec.mx>
2020-03-28 14:43:25 -0600

    Initial Commit

---
+++ Tareas-CoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tareas-CoreData
+//
+//  Created by David A Cantú Delgado on 28/03/20.
+//  Copyright © 2020 Tec de Monterrey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
